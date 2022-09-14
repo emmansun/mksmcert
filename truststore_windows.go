141d4d1ffa39599147588fcf4f4a763931791799
Sun Yimin <emmansun@users.noreply.github.com>
2022-09-14 16:46:20 +0800

    fix SA1019

--- truststore_windows.go
+++ truststore_windows.go
@@ -7,7 +7,6 @@ package main
 import (
 	"encoding/pem"
 	"fmt"
-	"io/ioutil"
 	"math/big"
 	"os"
 	"path/filepath"
@@ -35,7 +34,7 @@ var (
 
 func (m *mkcert) installPlatform() bool {
 	// Load cert
-	cert, err := ioutil.ReadFile(filepath.Join(m.CAROOT, rootName))
+	cert, err := os.ReadFile(filepath.Join(m.CAROOT, rootName))
 	fatalIfErr(err, "failed to read root certificate")
 	// Decode PEM
 	if certBlock, _ := pem.Decode(cert); certBlock == nil || certBlock.Type != "CERTIFICATE" {
